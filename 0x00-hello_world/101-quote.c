bbed1f52666f9169d6a57d1b7a7aeb25f20ff6af
donawg <donw.giorgis@gmail.com>
2022-12-08 14:48:59 -0800

    betty 111111111111111111

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
+			59, 1, stderr);
 	return (1);
 }
