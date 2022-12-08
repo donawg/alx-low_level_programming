06342c7e883173a2b04e5b98352a880a264906f7
donawg <donw.giorgis@gmail.com>
2022-12-08 14:47:52 -0800

    now with a new line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,6 @@
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-	fwrite(quote, sizeof(quote), 1, stderr);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
 	return (1);
 }
