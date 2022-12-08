216d1f03122026cf36ff0005e50803386df99a4b
donawg <donw.giorgis@gmail.com>
2022-12-08 13:51:45 -0800

    fixed syntax and included stdio.h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -5,6 +7,6 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n"
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
