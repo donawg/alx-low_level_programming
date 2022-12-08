0271150272b8f331830c475fbf1becd46b3ef3fe
donawg <donw.giorgis@gmail.com>
2022-12-08 14:10:24 -0800

    finished, not sure about prev error

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
