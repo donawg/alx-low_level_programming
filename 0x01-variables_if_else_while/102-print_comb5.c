971362ee171b242cf91c46a9df13b33dbe3da80c
donawg <donw.giorgis@gmail.com>
2022-12-12 16:25:39 -0800

    weird fix, return in loop

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (i = 48; i < 58; i++)
 	{
-		for (j = 48; j < 57; j++)
+		for (j = 48; j < 58; j++)
 		{
 			for (k = i; k < 58; k++)
 			{
@@ -28,11 +28,13 @@ int main(void)
 						putchar(44);
 						putchar(32);
 					}
+					else 
+					{
+						putchar('\n');
+						return (0);
+					}
 				}
 			}
 		}
 	}
-	putchar('\n');
-
-	return (0);
 }
