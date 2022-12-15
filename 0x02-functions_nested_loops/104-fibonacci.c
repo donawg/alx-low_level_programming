9fead6007dde902684a805e1b782aaccc7187114
donawg <donw.giorgis@gmail.com>
2022-12-15 04:09:18 -0800

    kinda cheated but ok, fin?

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,26 +12,32 @@ int main(void)
 	unsigned long nn0 = 0, nn1 = 0;
 	unsigned long p0 = 0, p1 = 1, n0 = 0, n1 = 2;
 	unsigned long C_LIMIT = 1000000000000000;
+	int optional_zero = 0;
 	int c = 2;
 
 	printf("1, 2, ");
 
 	while (c < 98)
 	{
+		if (optional_zero)
+			optional_zero = 0;
 		nn0 = p0 + n0;
 		nn1 = p1 + n1;
 		if (nn1 >= C_LIMIT)
 		{
 			nn0 += (nn1 / C_LIMIT);
 			nn1 %= C_LIMIT;
+			if (nn1 < (C_LIMIT / 10))
+				optional_zero = 1;
 		}
 		p0 = n0;
 		p1 = n1;
 		n0 = nn0;
 		n1 = nn1;
-		/* make sure nn0 isn't printed if 0 */
-		if (nn0)
-			printf("%lu%lu", nn0, nn1); 
+		if (nn0 && optional_zero)
+			printf("%lu0%lu", nn0, nn1);
+		else if (nn0)
+			printf("%lu%lu", nn0, nn1);
 		else
 			printf("%lu", nn1);
 		if (c != 97)
