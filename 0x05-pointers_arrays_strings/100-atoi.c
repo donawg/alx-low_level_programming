1c6f63f5d7e9b838850cf8a862a2b2fe37b3b5c3
donawg <donw.giorgis@gmail.com>
2022-12-20 10:08:52 -0800

    now counts all minuses, odd amount meants negative number

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -24,31 +24,35 @@ int _strlen(char *s)
  */
 int _atoi(char *s)
 {
-	int s_l, p, i, start;
+	int s_l, p, i, start, m_count;
 	int s_int = 0;
 
 	s_l = _strlen(s) - 1;
 	i = 0;
 	start = -1;
+	m_count = 0;
 	while (i <= s_l)
 	{
 		if (s[i] >= 48 && s[i] < 58)
+		{
 			start = i;
+		}
 		else
+		{
 			if (start >= 0)
 				break;
+			if (s[i] == '-')
+				m_count += 1
+		}
 		i++;
 	}
 	p = 1;
 	while (start >= 0)
 	{
-		if (s[start] == '-')
+		if (!(s[start] >= 48 && s[start] < 58))
 		{
-			s_int = 0 - s_int;
-			break;
-		}
-		else if (!(s[start] >= 48 && s[start] < 58))
-		{
+			if (m_count % 2)
+				s_int = 0 - s_int;
 			break;
 		}
 		else
