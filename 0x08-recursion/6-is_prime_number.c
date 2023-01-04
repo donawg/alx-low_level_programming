03a192bc637b78f21d47a009333d8df93fa18b5d
donawg <donw.giorgis@gmail.com>
2023-01-04 12:39:05 -0800

    meager attempt at task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * is_primt_number - figures out if a number is prime
+ * @n: number to test
+ *
+ * Return: 1 if prime number, 0 other wise
+ */
+int is_prime_number(int n)
+{
+	return (1);
+}
