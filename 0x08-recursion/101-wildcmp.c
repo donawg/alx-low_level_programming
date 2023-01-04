0e866e9a4516ace015aabf2c55bf239c5bc50553
donawg <donw.giorgis@gmail.com>
2023-01-04 14:01:47 -0800

    skips redundant wildcards

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -20,7 +20,11 @@ int wildcmp(char *s1, char *s2)
 	if (c1 == c2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (c2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
 		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
 			return (1);
+	}
 	return (0);
 }
