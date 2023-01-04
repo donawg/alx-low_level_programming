2832819a9c27d44c405590e8a43f69d2ae44cdc6
donawg <donw.giorgis@gmail.com>
2023-01-04 13:55:27 -0800

    moved around tests so longer one is checked first

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -20,7 +20,7 @@ int wildcmp(char *s1, char *s2)
 	if (c1 == c2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (c2 == '*')
-		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
 			return (1);
 	return (0);
 }
