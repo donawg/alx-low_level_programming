2215fd2019f19b3c7dbc447588e5464ac055a020
donawg <donw.giorgis@gmail.com>
2023-01-10 22:06:29 -0800

    included c libraries

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -13,10 +15,10 @@ char *create_array(unsigned int size, char c)
 	int i;
 
 	if (!size)
-		return ('\0');
+		return (NULL);
 	array = malloc(sizeof(char) * size);
 	if (!array)
-		return ('\0');
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		*(array + i) = c;
