53a335d7468bc8dc60f14bfad7024cb5d24b153f
donawg <donw.giorgis@gmail.com>
2023-01-10 22:00:27 -0800

    started with new project, finished 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array with all items being a specific char
+ * @size: size of array
+ * @c: char to set all array items to
+ *
+ * Return: Pointer to array. Or NULL if fail.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	int i;
+
+	if (!size)
+		return (NULL);
+	array = malloc(sizeof(char) * size);
+	if (!array)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		*(array + i) = c;
+	return (array);
+}
