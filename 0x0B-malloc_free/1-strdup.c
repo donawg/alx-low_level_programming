d0d5dff06aa83d6054cb6a9e9a991e9eeace69f2
donawg <donw.giorgis@gmail.com>
2023-01-10 22:23:41 -0800

    finished with 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - duplicate a string
+ * @str: String to duplicate
+ *
+ * Return: Pointer to new string. NULL if fail.
+ */
+char *_strdup(chat *str)
+{
+	int len;
+	int i;
+	char *strdup;
+
+	if (!str)
+		return (NULL);
+
+	len = 0;
+	while (1)
+	{
+		if (str[len] == '\0')
+			break;
+		else
+			len++;
+	}
+	strdup = malloc(sizeof(char) * (len + 1));
+	if (!strdup)
+		return (NULL);
+
+	i = 0;
+	while (i <= len)
+	{
+		*(strdup + i) = *(str + i);
+		i++;
+	}
+	*(strdup + i) = '\0';
+
+	return (strdup);
+}
