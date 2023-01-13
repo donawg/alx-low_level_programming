7237cf508cce36714bb9e27005bbeac7467068a7
donawg <donw.giorgis@gmail.com>
2023-01-12 23:43:05 -0800

    succinct behaviour for NULL ptr being passed in

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,6 +16,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (old_size == new_size)
 		return (ptr);
+	if (!ptr)
+		return (malloc(new_size));
 	if (new_size == 0)
 	{
 		free(ptr);
