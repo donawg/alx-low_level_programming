ffa9d17ebbe1e91b98fe5f779fe96ea68d107936
donawg <donw.giorgis@gmail.com>
2023-01-12 23:39:59 -0800

    fin, mashaAllah Ta'al;a

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _realloc - reallocates memory block
+ * @ptr: memory to be reallocated
+ * @old_size: size of memory previously
+ * @new_size: new size of memory
+ *
+ * Return: pointer to new memory block
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+	int i, copy_size;
+
+	if (old_size == new_size)
+		return (ptr);
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	copy_size = old_size < new_size ? old_size : new_size;
+	new_ptr = malloc(new_size);
+	for (i = 0; i < copy_size; i++)
+		*((char *) new_ptr + i) = *((char *) ptr);
+	return (new_ptr);
+}
