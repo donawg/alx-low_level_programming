31e723b25bb589e64309487a47a94d98a0153dc3
donawg <donw.giorgis@gmail.com>
2023-01-21 23:44:21 -0800

    not including more libraries, one error to solve atm

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
-#include <stddef.h>
+#define size_t unsigned int
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
