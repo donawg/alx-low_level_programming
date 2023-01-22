ccddf5664dededc143052e32523963fe9d33dd4e
donawg <donw.giorgis@gmail.com>
2023-01-22 12:06:51 -0800

    mashaAllah!

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - prints a specific number of opcodes
+ * @argc: number of arguments passed in
+ * @argv: arguments passed in
+ *
+ * Return: 0 for success. 1 for incorrect argument count. 2 for negative bytes.
+ */
+int main(int argc, char *argv[])
+{
+	int i, bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(10);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < bytes; i++)
+	{
+		if (((char *) main) + i == (char *) next)
+			break;
+		printf("%02x%c", *(((unsigned char *) main) + i),
+				 (i == bytes - 1) ? '\n' : ' ');
+	}
+
+	return (0);
+}
+
+int next(void)
+{
+	return (0);
+}
