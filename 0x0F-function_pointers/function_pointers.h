1930b41adf693a5f01fd9b68edd48d4abdf5c833
donawg <donw.giorgis@gmail.com>
2023-01-21 23:53:59 -0800

    goign back sadly

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
-#define size_t int
+#include <stddef.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
