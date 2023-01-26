b9d738537909726f7422ffb7a21791cdf68a7603
donawg <donw.giorgis@gmail.com>
2023-01-26 12:40:13 -0800

    fixed betty, weird backslash bugs

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
 void __attribute__ ((constructor)) pre_main()
 {
 	printf(
-"You're beat! and yet, you must allow,\n\I bore my house upon my back!\n"
+"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
 );
 }
