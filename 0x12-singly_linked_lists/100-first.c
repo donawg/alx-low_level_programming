cf5cdeabd0f2f6e7627eaa9cd3e575f45ae1ae01
donawg <donw.giorgis@gmail.com>
2023-01-26 12:10:33 -0800

    betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,5 +5,7 @@
  */
 void __attribute__ ((constructor)) pre_main()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf(
+"You're beat! and yet, you must allow,\n\I bore my house upon my back!\n"
+);
 }
