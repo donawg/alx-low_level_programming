2141e33c711031269c6b28044b718b4bf07c0781
donawg <donw.giorgis@gmail.com>
2023-01-26 22:30:48 -0800

    removed start since apparently gcc links it already(?)

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,33 +1,19 @@
- SECTION .data
+SECTION .data
 
-msg:    db "Hello, Holberton", 0
+	msg: db "Hello, Holberton", 0
+	fmt: db "%s", 10, 0
 
-fmt:    db "%s", 10, 0
+SECTION .text
 
-
-
-        SECTION .text
-
-        extern printf
-
-        global main, _start
+	extern printf
+	global main
 
 main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-_start:
-
-        mov esi, msg    
-
-        mov edi, fmt    
-
-        mov eax, 0      
-
-        call printf
-
-
-
-        mov ebx, 0      
-
-        mov eax, 1  
-
-        int 0x80      
+	mov ebx, 0
+	mov eax, 1
+	int 0x80
