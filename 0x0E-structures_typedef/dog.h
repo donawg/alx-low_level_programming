f50d08a1b66645301767058736c06f4d371334e4
donawg <donw.giorgis@gmail.com>
2023-01-28 07:15:49 -0800

    start

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - doggo
+ * @name: name of doggo
+ * @age: age of doggo
+ * @owner: owner of person allowed to have doggo
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
