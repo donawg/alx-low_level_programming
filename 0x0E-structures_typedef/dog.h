dbee2146fed31bbcc0a76941d6c2cbce132539f1
donawg <donw.giorgis@gmail.com>
2023-01-28 07:22:06 -0800

    typedeffed the doggos

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,10 +8,14 @@
  * @age: age of doggo
  * @owner: owner of person allowed to have doggo
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
